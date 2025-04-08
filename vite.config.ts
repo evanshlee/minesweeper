c140101d86111eed195a525c20ef66d0dfc9d0c8
sounmind <devleesche@gmail.com>
2025-04-08 08:39:12 -0400

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/minesweeper/",
   test: {
     environment: "happy-dom",
     setupFiles: "./src/setupTests.tsx",
